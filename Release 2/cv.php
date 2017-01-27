8cdd5545c03e822a935323247fe4a35e4c7c4a77
Natasha George <natashaa.george3@gmail.com>
2017-01-27 14:48:39 +0100

    jgv

--- Release 2/cv.php
+++ Release 2/cv.php
@@ -10,7 +10,7 @@
 <meta charset="utf-8" />
 </head>
 
-<body> 
+<body>
 
 <!--mobilemenu starts here-->
 <div class="sidebar">
@@ -60,8 +60,17 @@
 </div>
 </nav>
 <!--Meny1 avslutas-->
-<section class="maincv">
-
+section class="maincv">
+<article class="cv-content">
+<div contenteditable="true">
+<p>Vad är Lorem Ipsum?
+Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden, utan även övergången till elektronisk typografi utan större förändringar. Det blev allmänt känt på 1960-talet i samband med lanseringen av Letraset-ark med avsnitt av Lorem Ipsum, och senare med mjukvaror som Aldus PageMaker.</p>
+<p>Vad är Lorem Ipsum?
+Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden, utan även övergången till elektronisk typografi utan större förändringar. Det blev allmänt känt på 1960-talet i samband med lanseringen av Letraset-ark med avsnitt av Lorem Ipsum, och senare med mjukvaror som Aldus PageMaker.</p>
+<p>Vad är Lorem Ipsum?
+Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden, utan även övergången till elektronisk typografi utan större förändringar. Det blev allmänt känt på 1960-talet i samband med lanseringen av Letraset-ark med avsnitt av Lorem Ipsum, och senare med mjukvaror som Aldus PageMaker.</p>
+</div>
+</article>
 </section>
 
 <footer id="footer"></footer>
